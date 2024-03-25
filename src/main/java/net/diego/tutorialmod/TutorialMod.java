8bf04a929bbd33b227fff7d96ae9d0d4a8dd7468
Diego Seminario <practicas.0062@sesitdigital.com>
2024-03-24 21:56:36 -0500

    Initial commit

---
+++ src/main/java/net/diego/tutorialmod/TutorialMod.java
@@ -0,0 +1,14 @@
+package net.diego.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	@Override
+	public void onInitialize() {
+
+	}
+}
